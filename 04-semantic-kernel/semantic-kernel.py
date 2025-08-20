d949106789f74ff095c14cac7b58bb86bde3bbd3
Vinny Martins <martins.vds@outlook.com>
2025-08-20 12:08:17 -0600

    add lab 5

--- 04-semantic-kernel/semantic-kernel.py
+++ 04-semantic-kernel/semantic-kernel.py
@@ -43,46 +43,45 @@ async def process_expenses_data(prompt, expenses_data):
     async with (
         DefaultAzureCredential(
             exclude_environment_credential=True,
-            exclude_managed_identity_credential=True) as creds,
-        AzureAIAgent.create_client(
-            credential=creds
-        ) as project_client,
+            exclude_managed_identity_credential=True,
+        ) as creds,
+        AzureAIAgent.create_client(credential=creds) as project_client,
     ):
 
-    # Define an Azure AI agent that sends an expense claim email
-    expenses_agent_def = await project_client.agents.create_agent(
-        model= ai_agent_settings.model_deployment_name,
-        name="expenses_agent",
-        instructions="""You are an AI assistant for expense claim submission.
-                        When a user submits expenses data and requests an expense claim, use the plug-in function to send an email to expenses@contoso.com with the subject 'Expense Claim`and a body that contains itemized expenses with a total.
-                        Then confirm to the user that you've done so."""
-    )
+        # Define an Azure AI agent that sends an expense claim email
+        expenses_agent_def = await project_client.agents.create_agent(
+            model=ai_agent_settings.model_deployment_name,
+            name="expenses_agent",
+            instructions="""You are an AI assistant for expense claim submission.
+                            When a user submits expenses data and requests an expense claim, use the plug-in function to send an email to expenses@contoso.com with the subject 'Expense Claim`and a body that contains itemized expenses with a total.
+                            Then confirm to the user that you've done so.""",
+        )
 
-    # Create a semantic kernel agent
-    expenses_agent = AzureAIAgent(
-        client=project_client,
-        definition=expenses_agent_def,
-        plugins=[EmailPlugin()]
-    )
+        # Create a semantic kernel agent
+        expenses_agent = AzureAIAgent(
+            client=project_client,
+            definition=expenses_agent_def,
+            plugins=[EmailPlugin()],
+        )
 
-    # Use the agent to process the expenses data
-    # If no thread is provided, a new thread will be
-    # created and returned with the initial response
-    thread: AzureAIAgentThread | None = None
-    try:
-        # Add the input prompt to a list of messages to be submitted
-        prompt_messages = [f"{prompt}: {expenses_data}"]
-        # Invoke the agent for the specified thread with the messages
-        response = await expenses_agent.get_response(prompt_messages, thread=thread)
-        # Display the response
-        print(f"\n# {response.name}:\n{response}")
-    except Exception as e:
-        # Something went wrong
-        print (e)
-    finally:
-        # Cleanup: Delete the thread and agent
-        await thread.delete() if thread else None
-        await project_client.agents.delete_agent(expenses_agent.id)
+        # Use the agent to process the expenses data
+        # If no thread is provided, a new thread will be
+        # created and returned with the initial response
+        thread: AzureAIAgentThread | None = None
+        try:
+            # Add the input prompt to a list of messages to be submitted
+            prompt_messages = [f"{prompt}: {expenses_data}"]
+            # Invoke the agent for the specified thread with the messages
+            response = await expenses_agent.get_response(prompt_messages, thread=thread)
+            # Display the response
+            print(f"\n# {response.name}:\n{response}")
+        except Exception as e:
+            # Something went wrong
+            print(e)
+        finally:
+            # Cleanup: Delete the thread and agent
+            thread.delete() if thread else None
+            await project_client.agents.delete_agent(expenses_agent.id)
 
 
 # Create a Plugin for the email functionality
